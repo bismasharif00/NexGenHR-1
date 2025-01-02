384cbd4827b96c461ccf0b8022915c5296a63d18
Shahzaib-Rajput0 <shahzaibamjad02@gmail.com>
2025-01-03 00:45:42 +0500

    db name correction

--- beckend/server.js
+++ beckend/server.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
     host: "hrsystem.cbkwgweoa98s.eu-north-1.rds.amazonaws.com",
     user: 'admin',
     password: 'f2021266024',
-    database: 'sys'
+    database: 'HrSystem'
 });
 
 
